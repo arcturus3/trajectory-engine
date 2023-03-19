65d543f0b711ccad559f35f6cbf75cd70f3810f5
Arti Schmidt <artischmidt3@gmail.com>
2023-03-19 01:18:24 -0400

    Finish simple trajectory server

--- server.py
+++ server.py
@@ -1,12 +1,51 @@
-import time
 import zmq
+import RapidQuadrocopterTrajectories.Python.quadrocoptertrajectory as quadtraj
+import generator
+
+def generate(args):
+    waypoints = args['waypoints']
+    trajectory = generator.generate(
+        5,
+        waypoints[0]['position'],
+        (0, 0, 0),
+        (0, 0, 0),
+        waypoints[1]['position'],
+        None,
+        None
+    )
+    trajectory_id = len(trajectories)
+    trajectories.append(trajectory)
+    return {
+        'trajectory_id': trajectory_id
+    }
+
+def query(args):
+    trajectory_id = args['trajectory_id']
+    time = args['time']
+    trajectory = trajectories[trajectory_id]
+    return {
+        # reparameterize by arc length instead?
+        'position': trajectory.get_position(time)
+    }
+
+def handle_request(request):
+    handlers = {
+        'generate': generate,
+        'query': query
+    }
+    command = request['command']
+    args = request['args']
+    response = handlers[command](args)
+    return response
+
+trajectories: list[quadtraj.RapidTrajectory] = []
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind('tcp://*:5555')
-
 while True:
-    message = socket.recv()
-    print(f'received message: {message}')
-    time.sleep(1)
-    socket.send_string('connection established')
+    request = socket.recv_json()
+    print('received:', request)
+    response = handle_request(request)
+    socket.send_json(response)
+    print('sent:', response)
---
+++ waypoint.py
@@ -0,0 +1,3 @@
+class Waypoint:
+    def __init__(self, position):
+        self.position = position
