b275d02c600ba9081fcc7324bdf7d0651cfe9576
Arti Schmidt <artischmidt3@gmail.com>
2023-03-19 15:20:47 -0400

    Fix API

--- server.py
+++ server.py
@@ -2,40 +2,41 @@ import zmq
 import RapidQuadrocopterTrajectories.Python.quadrocoptertrajectory as quadtraj
 import generator
 
-def generate(args):
-    waypoints = args['waypoints']
+def generate(message):
+    waypoints = message['waypoints']
     trajectory = generator.generate(
         5,
         waypoints[0]['position'],
         (0, 0, 0),
         (0, 0, 0),
         waypoints[1]['position'],
-        None,
+        (0, 0, 5),
         None
     )
     trajectory_id = len(trajectories)
     trajectories.append(trajectory)
     return {
+        'message_type': 'generate_response',
         'trajectory_id': trajectory_id
     }
 
-def query(args):
-    trajectory_id = args['trajectory_id']
-    time = args['time']
+def query(message):
+    trajectory_id = message['trajectory_id']
+    time = message['time']
     trajectory = trajectories[trajectory_id]
     return {
+        'message_type': 'query_response',
         # reparameterize by arc length instead?
-        'position': trajectory.get_position(time)
+        'position': tuple(trajectory.get_position(time))
     }
 
 def handle_request(request):
     handlers = {
-        'generate': generate,
-        'query': query
+        'generate_request': generate,
+        'query_request': query
     }
-    command = request['command']
-    args = request['args']
-    response = handlers[command](args)
+    message_type = request['message_type']
+    response = handlers[message_type](request)
     return response
 
 trajectories: list[quadtraj.RapidTrajectory] = []
